204cc5287895b5c5de3a171c56e291cdda256baa
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-07-03 15:42:51 +0800

    Finished species sales graph. Feel free to edit

--- js/species_sale.js
+++ js/species_sale.js
@@ -25,23 +25,21 @@ function importSpeciesSales() {
         }
         var jsonString = multjsonparser(species, count, ["species", "count"]);
         var newData = toJson(jsonString);
-        console.log(newData);
         /*---------------------------------------------------------------------------------------*/
 
-        var margin = { top: 20, right: 10, bottom: 100, left: 90 },
+        var margin = { top: 20, right: 10, bottom: 100, left: 100 },
             width = 430 - margin.right - margin.left,
-            height = 280 - margin.top - margin.bottom;
+            height = 230 - margin.top - margin.bottom;
 
 
-        var canvas = d3.select("#hello").append("svg")
+        var canvas = d3.select("#spcs_sales").append("svg")
             .attr("width", width + margin.right + margin.left)
             .attr("height", height + margin.top + margin.bottom)
             .attr("class", "species_sale")
-        // .append("g")
+            .append("g")
         // .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
-
         var xScale = d3.scaleLinear()
-            .domain([0, d3.max(count)])
+            .domain([0, Math.ceil(d3.max(count) / 100) * 100])
             .range([0, width]);
 
         var yScale = d3.scaleBand()
@@ -57,18 +55,17 @@ function importSpeciesSales() {
 
         var xg = canvas.append("g")
             .attr("class", "xAxis")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+            .attr("transform", "translate(" + margin.left + ", " + (margin.top - 1) + ")")
             .call(xAxis)
             .append("text")
-            // .attr("transform", "rotate(90)")
             .attr("y", 5)
             .attr("dy", "-0.8em")
-            .attr("text-anchor", "middle")
+            .attr("text-anchor", "end")
             .text("Count")
 
         var yg = canvas.append("g")
             .attr("class", "yAxis")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+            .attr("transform", "translate(" + (margin.left - 1) + ", " + margin.top + ")")
             .call(yAxis)
             .selectAll("text")
             .style("text-anchor", "end")
@@ -76,101 +73,114 @@ function importSpeciesSales() {
             .attr("dy", "-0.55em")
             .attr("y", 10)
             .selectAll(".tick text")
-        // .attr("transform", "rotate()")
+        canvas.append('g')
+            .attr('class', 'grid')
+            .attr("transform", "translate(" + (margin.left) + ", " + (margin.top - 1) + ")")
+            .call(d3.axisBottom()
+                .scale(xScale)
+                .tickSize(height + margin.top + margin.bottom, 0, 0)
+                .tickFormat(''))
 
-
-        var rect = canvas.selectAll("rect")
+        const bar = canvas.selectAll()
             .data(newData)
             .enter()
-            .append("rect")
+            .append("g")
+
+        const rect = bar.append("rect")
             .attr("width", function (d) {
-                return xScale(d.count)
+                return 0;
             })
             .attr("y", function (d) {
-                return yScale(d.species)
+                return yScale(d.species);
             })
             .attr("height", yScale.bandwidth())
-            .attr("fill", "steelblue")
+            .attr("fill", "pink")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .on("mouseover", function () {
-                tooltip.style("display", null);
+
+
+
+        rect.transition()
+            .duration(1000)
+            .attr("width", function (d) {
+                return xScale(d.count);
             })
-            .on("mouseout", function () {
-                tooltip.style("display", "none");
+            .on("end", function () {
+                rect.on("mouseover", function (actual, i) {
+                    d3.selectAll(".value")
+                        .attr("visibility", "hidden")
+
+                    d3.select(this)
+                        .transition()
+                        .duration(300)
+                        .attr("y", (a) => yScale(a.species) - 2)
+                        .style("fill", "rgb(255,255,150)")
+                        .attr("height", yScale.bandwidth() + 4)
+                        .attr("opacity", 0.7);
+
+                    const x = xScale(actual.count)
+                    var line = canvas.append("line")
+                        .attr("id", "limit")
+                        .attr("x1", margin.left + x)
+                        .attr("x2", margin.left + x)
+                        .attr("y1", margin.top + 0)
+                        .attr("y2", margin.top + margin.bottom + height)
+                        .attr("stroke", "red")
+
+
+
+
+                    var diff = bar.append('text')
+                        .attr('class', 'divergence')
+                        .attr("y", (a) => margin.top + yScale(a.species) + yScale.bandwidth() - 5)
+                        .attr("x", (a) => margin.left + xScale(a.count) - 4)
+                        .attr('text-anchor', 'end')
+                        .attr("fill", function (a) {
+                            const divergence = (a.count - actual.count).toFixed()
+                            if (divergence > 0) return "green"
+                            else return "red"
+                        })
+                        .text((a, idx) => {
+                            const divergence = (a.count - actual.count).toFixed()
+                            let text = ''
+                            if (divergence > 0) text += '+'
+                            text += `${divergence}`
+                            return idx !== i ? text : '';
+                        })
+
+
+                })
+                    .on("mouseout", function () {
+                        d3.selectAll(".value")
+                            .attr("visibility", "visible")
+
+                        d3.select(this)
+                            .transition()
+                            .duration(300)
+                            .style("fill", "rgb(255,255,102)")
+                            .attr("y", (a) => yScale(a.species))
+                            .attr("height", yScale.bandwidth())
+                            .attr("opacity", 1);
+
+                        canvas.selectAll("#limit").remove()
+                        canvas.selectAll(".divergence").remove()
+                    })
             })
-            .on("mousemove", function (d) {
-                var xPos = d3.mouse(this)[0] + 10;
-                var yPos = d3.mouse(this)[1] - 20;
-                tooltip.attr("transform", "translate(" + xPos + ", " + yPos + ")");
-                tooltip.select("text").text(d.species + " : " + d.count);
+
+
+        const tooltip = bar.append("text")
+            .attr("class", "value")
+            .attr("text-anchor", "end")
+            .attr("fill", "#2884cf")
+            .attr("y", (a) => margin.top + yScale(a.species) + yScale.bandwidth() - 5)
+            .attr("x", (a) => margin.left + 22)
+            .text((a) => a.count)
+
+
+        tooltip.transition()
+            .duration(1100)
+            .attr("fill", "gray")
+            .attr("transform", function (d) {
+                return "translate(" + (xScale(d.count) - 25) + "," + 0 + ")";
             })
-
-
-        var tooltip = canvas.append("g")
-            .attr("class", "tooltip")
-            .style("display", "none")
-
-        tooltip.append("text")
-            .attr("x", 15)
-            .attr("dy", "1.2em")
-            .style("font-size", "1.25em")
-            .attr("font-weight", "bold")
-
-
-
-
-
-        // var margin = { top: 50, right: 20, left: 50, bottom: 20 },
-        //     width = 600,
-        //     height = 600;
-
-        // var yScale = d3.scaleLinear()
-        //     .domain([0, d3.max(count)])
-        //     .range([width, 0]);
-
-        // var yAxis = d3.axisLeft()
-        //     .scale(yScale);
-
-        // var xScale = d3.scaleBand()
-        //     .range([0, width])
-        //     .domain(newData.map((s) => s.language))
-        //     .padding(0.4)
-
-
-        // var canvas = d3.select("#hello").append("svg")
-        //     .attr("width", width)
-        //     .attr("height", height)
-        //     .attr("class", "species_sale")
-        //     .append("g");
-
-
-
-        //     var rect = canvas.selectAll("rect")
-        //     .data(newData)
-        //     .enter()
-        // .append("rect")
-        // .attr("width", 50)
-        // .attr("x", function (d, i) {
-        //     return i * 75;
-        // })
-        // .attr("height", function (d) {
-        //     return yScale(d.count);
-        // })
-        // .attr("y", function(d){
-        //     return height - yScale(d.count);
-        // })
-        // .attr("fill", "steelblue")
-        //     .attr("transform", "translate("+margin.left+","+margin.top+")");
-
-        // canvas.append("g")
-        //     .attr("transform", "translate("+margin.left+","+margin.top+")")
-        //     .call(yAxis);
-
-        // var myChart = new dimple.chart(svg, newData);
-        // myChart.setBounds(60, 30, 510, 305);
-        // var x = myChart.addCategoryAxis("y", "Hi");
-        // myChart.addMeasureAxis("x", "Hello");
-        // myChart.addSeries(null, dimple.plot.bar);
-        // myChart.draw();
     })
 }
